0d46ba181fac8eb804939f3eec5918e97c7501e1
Kelvin Chin <chinkaiwen@yahoo.com>
2023-07-29 14:08:19 +0800

    Completed exercise for hashmap

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -40,6 +40,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.insert(team_1_name, { name: team_1_name, goals_scored: });
+        scores.insert(team_2_name, team_2_score);
     }
     scores
 }
